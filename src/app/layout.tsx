e60d32340f9af5c70ff8f7f52b25e1740e0207db
Javier Camargo <javcbdev@outlook.com>
2025-11-08 14:39:07 -0500

    feat: add Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar/Navbar";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
         <Navbar />
-        {children}
+        <main className="grow px-4 py-6">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+  return (
+    <footer className="bg-gray-800 px-3 py-4">
+      <div className="text-center">
+        <p className="text-md font-medium tracking-wide text-white">© 2025 DELOSI</p>
+      </div>
+    </footer>
+  )
+}
